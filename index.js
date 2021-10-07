9ea3031ce860b2ac28ed5e53461703dfa474f80f
prath-dev <73606133+prath-dev@users.noreply.github.com>
2021-10-07 14:03:05 +0530

    img
    image added

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
    
 const birthdate = document.querySelector("#birthdate"); 
 const luckyno = document.querySelector("#lucky-number");
-const resultBtn = document.querySelector(".btn");
+const resultBtn = document.querySelector(".btn"); 
+const img = document.querySelector('#img');
+const sadimgUrl = 'https://thumbs.dreamstime.com/b/children-boy-thinking-smile-idea-chalkboard-cute-kid-imagine-classroom-space-your-text-education-concept-books-102349052.jpg';
+const happyimgUrl = 'https://img.freepik.com/free-vector/happy-young-couple-having-fun-girl-guy-dancing-party-celebrating-good-news-flat-illustration_74855-10820.jpg?size=338&ext=jpg'; 
 const output = document.querySelector(".output");  
 resultBtn.addEventListener('click', checkluckynum ) ;  
 
@@ -18,11 +21,14 @@ resultBtn.addEventListener('click', checkluckynum ) ;
          sum = sum + Number(BOD.charAt(i)) 
       }
         if (sum%LuckyNUM==0) {
-           output.innerText= "your birthdate is lucky" ; 
+           output.innerText= "your birthdate is lucky" ;  
+           img.innerHTML = `<img src=${happyimgUrl} />`
 
         } 
         else {
            output.innerText= LuckyNUM +"is not that lucky" ;
+           img.innerHTML = `<img src=${sadimgUrl} />`;
+
         }  
       } 
 
